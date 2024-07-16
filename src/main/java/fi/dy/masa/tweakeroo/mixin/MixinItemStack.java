5ab5f0118d35fdf3208c31e85b2fcc205b30ab31
Liyan Zhao <return65535@qq.com>
2024-07-16 12:52:18 +0800

    fix (#24)
    Co-authored-by: Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>

--- src/main/java/fi/dy/masa/tweakeroo/mixin/MixinItemStack.java
+++ src/main/java/fi/dy/masa/tweakeroo/mixin/MixinItemStack.java
@@ -1,16 +1,16 @@
 package fi.dy.masa.tweakeroo.mixin;
 
+import fi.dy.masa.malilib.util.InventoryUtils;
+import fi.dy.masa.tweakeroo.config.FeatureToggle;
+import net.minecraft.block.ShulkerBoxBlock;
+import net.minecraft.item.BlockItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import net.minecraft.component.ComponentMap;
-import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.entry.RegistryEntry;
-import fi.dy.masa.tweakeroo.util.IItemStackLimit;
 
 @Mixin(ItemStack.class)
 public abstract class MixinItemStack
@@ -18,24 +18,15 @@ public abstract class MixinItemStack
     @Shadow
     public abstract Item getItem();
 
-    @Shadow public abstract ComponentMap getComponents();
-
-    @Shadow public abstract RegistryEntry<Item> getRegistryEntry();
-
     @Inject(method = "getMaxCount", at = @At("RETURN"), cancellable = true)
     public void getMaxStackSizeStackSensitive(CallbackInfoReturnable<Integer> cir)
     {
-        int component = this.getComponents().getOrDefault(DataComponentTypes.MAX_STACK_SIZE, 1);
-        int tweakeroo = ((IItemStackLimit) this.getItem()).getMaxStackSize((ItemStack) (Object) this);
-
-        if (component <= this.getRegistryEntry().value().getMaxCount() &&
-            tweakeroo != 64)
+        if (FeatureToggle.TWEAK_SHULKERBOX_STACKING.getBooleanValue() &&
+            this.getItem() instanceof BlockItem block &&
+            block.getBlock() instanceof ShulkerBoxBlock &&
+            InventoryUtils.shulkerBoxHasItems((ItemStack) (Object) this) == false)
         {
-            cir.setReturnValue(component);
-        }
-        else
-        {
-            cir.setReturnValue(Math.max(component, tweakeroo));
+            cir.setReturnValue(64);
         }
     }
 }
